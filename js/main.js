3ac9980600a334923a3797303cc66c54edc95295
modupe <adewalebose15@gmail.com>
2020-07-08 10:46:01 +0100

    Add form

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 let myLibrary = [
-    
-
+       
 ];
 
-function Book() {
-  // the constructor...
+function Book(name, author, pages, read) {
+this.name = name;
+this.author = author;
+this.pages = pages;
+this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
   // do stuff here
+   const newBook = new Book(name, author, pages, read)
+
 }
 
 function render(){
-    for (let i = 0; i < myLibrary.length; i++){
-        let current_table = document.getElementById('table').innerHTML
+    // for (let i = 0; i < myLibrary.length; i++){
+    //     let current_table = document.getElementById('table').innerHTML
+    //     let newBook = `<tr>\n<td>${myLibrary[i].name}</td>
+    //   </tr>`
+    //   document.getElementById('table').innerHTML = current_table + newBook
+    // }
 
-    }
+  
 }
