51936dbe56de32138db58706d90a3292be5bf139
SpaYco <zizou.aziz101@gmail.com>
2020-07-08 09:50:20 +0100

    added the page desgin

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let myLibrary = [
+    
+
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function render(){
+    for (let i = 0; i < myLibrary.length; i++){
+        let current_table = document.getElementById('table').innerHTML
+
+    }
+}
